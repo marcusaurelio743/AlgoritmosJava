adf93bf90dd6def30c8c8f77ce05937b29d9a86e
marcus aurelio <marcusaurelio743@gmail.com>
2025-01-09 14:52:40 -0300

    identação de codigo

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -3,7 +3,9 @@ package fundamentos;
 public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
-		System.out.println("meu primeiro programa");
+		
+		System.out.println("meu primeiro programa #1");
+		System.out.println("meu primeiro programa #2");
 		
 	}
 
