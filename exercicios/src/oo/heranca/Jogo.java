689e470c06a3170055c6688d2206833ef822af7a
marcus aurelio <marcusaurelio743@gmail.com>
2025-01-25 19:47:54 -0300

    conceitos de herança

--- exercicios/src/oo/heranca/Jogo.java
+++ exercicios/src/oo/heranca/Jogo.java
@@ -3,23 +3,27 @@ package oo.heranca;
 public class Jogo {
 
 	public static void main(String[] args) {
-		Jogador j1 = new Monstro();
-		j1.x=10;
-		j1.y=10;
+		Jogador monstro = new Monstro();
+		monstro.x=10;
+		monstro.y=10;
 		
-		Jogador j2 = new Heroi();
-		j2.x=10;
-		j2.y=12;
-		
-		
-		System.out.println(j1.vida);
-		System.out.println(j2.vida);
-		j1.atacar(j2);
-		j2.atacar(j1);
+		Jogador heroi = new Heroi();
+		heroi.x=10;
+		heroi.y=11;
 		
 		
-		System.out.println(j2.vida);
-		System.out.println(j1.vida);
+		System.out.println("Monstro tem: "+monstro.vida);
+		System.out.println("Heroi tem: "+heroi.vida);
+		monstro.atacar(heroi);
+		heroi.atacar(monstro);
+		monstro.atacar(heroi);
+		heroi.atacar(monstro);
+		monstro.atacar(heroi);
+		heroi.atacar(monstro);
+		
+		
+		System.out.println("Monstro tem: "+monstro.vida);
+		System.out.println("Heroi tem: "+heroi.vida);
 	}
 
 }
