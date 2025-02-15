d60e4115a058d783790af42fa8d83b1add83a20e
marcus aurelio <marcusaurelio743@gmail.com>
2025-02-15 11:11:26 -0300

    desafio usando Stream

---
+++ exercicios/src/Stream/DesafioMap.java
@@ -0,0 +1,29 @@
+package Stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+
+public class DesafioMap {
+
+	public static void main(String[] args) {
+		 
+		List<Integer> numns =Arrays.asList(1,2,3,4,5,6,7,8,9);
+		
+		UnaryOperator<String> inverter = 
+				s-> new StringBuilder(s).reverse().toString();
+		Function<String, Integer> binarioParaInt = 
+				s->Integer.parseInt(s, 2);
+		
+		numns.stream()
+			.map(Integer::toBinaryString)
+			.map(inverter)
+			.map(binarioParaInt)
+			.forEach(System.out::println);
+		
+		
+	}
+
+}
